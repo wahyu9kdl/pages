f5e690e05fe8b859396bc0eed0f4a79fea1301bc
wahyu9kdl <84139744+wahyu9kdl@users.noreply.github.com>
2024-08-21 07:06:45 +0700

    Create adsense.js

---
+++ adsense.js
@@ -0,0 +1,5 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3961173026050537"
+     crossorigin="anonymous"></script>
+<script async custom-element="amp-auto-ads"
+        src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+</script>
